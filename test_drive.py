f3fd0656d3d200f9eb247e0568dd5d2a857e7817
nividic <gcaalbro@uci.edu>
2015-05-19 11:45:25 -0700

    generalization of the solute label

--- test_drive.py
+++ test_drive.py
@@ -4,25 +4,22 @@ from solvated_mixtures import *
 
 #In this particular instance I'll just look at six solutes/solvent mixtures (not an all-by-all combination) which are pre-specified
 #solute names
-solutes = ['phenol', 'toluene', 'benzene', 'methane', 'ethanol', 'naphthalene']
+solutes = ['phenol', 'toluene', 'benzene']
 #Solvent names
-solvents = ['cyclohexane', 'cyclohexane', 'cyclohexane', 'octanol', 'octanol', 'octanol']
+solvents = ['cyclohexane', 'cyclohexane', 'cyclohexane']
 
-#The mixture class taxes CAS strings so I need to pull these
+test1 = ['octanol', 'octanol', 'octanol']
 
-solute_CAS = [ openmoltools.cirpy.resolve( name, 'CAS') for name in solutes ]
-solvent_CAS = [ openmoltools.cirpy.resolve( name, 'CAS') for name in solvents ]
-
+test2 = ['methane','methane','methane']
 
 #Number of solute/solvent molecules
 Nsolu = 3
 Nsolv = 100
-
+Ntest1 = 4
+Ntest2 = 5
 
 #Construct systems
 for idx in range( len( solutes) ):
-    builder = MixtureSystem( [ solutes[idx], solvents[idx]], [ Nsolu, Nsolv], 'data/')  
+    builder = MixtureSystem( [ solutes[idx], solvents[idx], test1[idx],test2[idx]], [ Nsolu, Nsolv,Ntest1,Ntest2], 'data/',solute_index=3)  
     builder.run( just_build = True )
     builder.convert_to_gromacs()
-
-#Maybe down here I should try one which is a single solute in a binary mixture
