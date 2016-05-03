6409374bf6006007a5b0a02f4703760610e7237e
davidlmobley <dmobley@gmail.com>
2016-05-03 13:43:10 -0700

    Updating setup.py for release

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ if sys.version_info[:2] < (2, 7):
 
 
 ##########################
-VERSION = "0.2.2.dev0"
-ISRELEASED = False
+VERSION = "0.2.3"
+ISRELEASED = True
 __version__ = VERSION
 ##########################
 
