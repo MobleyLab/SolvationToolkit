f87f6677cf68810b073826b3810319df6e0bd119
davidlmobley <dmobley@gmail.com>
2016-06-10 10:37:07 -0700

    Remove license info from top of solvated_mixtures so it doesn't appear in help on the module

--- solvationtoolkit/solvated_mixtures.py
+++ solvationtoolkit/solvated_mixtures.py
@@ -1,27 +1,3 @@
-######################################################################
-# SolvationToolkit: A toolkit for setting up molecular simulations of mixtures
-# Copyright 2011-2016 UC Irvine and the Authors
-#
-# Authors: David Mobley and Gaetano Calabro
-# With thanks to Kyle Beauchamp, whose liquid_tools.py provided an initial 
-# basis for this code
-# (https://github.com/choderalab/LiquidBenchmark/blob/master/src/simulation/liquid_tools.py)
-# in April 2015
-#
-#This library is free software; you can redistribute it and/or
-#modify it under the terms of the GNU Lesser General Public
-#License as published by the Free Software Foundation; either
-#version 2.1 of the License, or (at your option) any later version.
-#
-#This library is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#Lesser General Public License for more details.
-#
-#You should have received a copy of the GNU Lesser General Public
-#License along with this library; if not, see <http://www.gnu.org/licenses/>.
-######################################################################
-
 
 import numpy as np
 import os,sys
