b24b0264e9cdcc0842f4ae08fd5fdb489c42b8a1
davidlmobley <dmobley@gmail.com>
2016-06-10 21:15:09 -0700

    Update setup.py for ongoing development

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ if sys.version_info[:2] < (2, 7):
 
 
 ##########################
-VERSION = "0.4.0"
-ISRELEASED = True
+VERSION = "0.4.1.dev0"
+ISRELEASED = False
 __version__ = VERSION
 ##########################
 
